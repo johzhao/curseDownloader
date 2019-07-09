8aeba711bc1c0f9d2cdd6118bcf51e529b549414
John Zhao <joh.zhao@gmail.com>
2019-07-09 14:10:44 +0800

    Use requests to prevent certification error

--- src/downloader/downloader.py
+++ src/downloader/downloader.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-import urllib3
+import requests
 
 from src.database.database import Database
 from src.exceptions import *
@@ -12,17 +12,17 @@ logger = logging.getLogger(__name__)
 class Downloader:
 
     def __init__(self):
-        self.http = None
+        self.session = requests.Session()
         self.database = Database()
         pass
 
     def __enter__(self):
-        self.http = urllib3.PoolManager()
+        self.session = requests.Session()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.http.clear()
-        self.http = None
+        self.session.close()
+        self.session = None
 
     def download_mod(self, project_id: str, file_id: str) -> (str, bytes):
         try:
@@ -33,15 +33,15 @@ class Downloader:
     def get_project_url(self, project_id: str) -> str:
         url = 'https://minecraft.curseforge.com/projects/{}'.format(project_id)
         logger.info('Request for name of the project {} by url {}'.format(project_id, url))
-        response = self.http.request('GET', url, redirect=False)
+        response = self.session.get(url, allow_redirects=False)
         return response.headers['location']
 
     def download_mod_file(self, project_id: str, project_url: str, file_id: str) -> (str, bytes):
         url = '{}/download/{}/file'.format(project_url, file_id)
-        response = self.http.request('GET', url)
-        filename = os.path.basename(response.geturl())
-        self.database.set_data(project_id, file_id, filename, response.data)
-        return filename, response.data
+        response = self.session.get(url)
+        filename = os.path.basename(response.url)
+        self.database.set_data(project_id, file_id, filename, response.content)
+        return filename, response.content
 
     def _download_mod_impl(self, project_id: str, file_id: str) -> (str, bytes):
         try:
